e99fae22228fb72f3e2042ef8b2f6465254dcc6c
James Draper <jd.unchecked@gmail.com>
2021-04-23 21:01:01 +0100

    Add transaction method to connection class.

--- src/Connection.php
+++ src/Connection.php
@@ -6,11 +6,16 @@ namespace Mycro;
 use PDOException;
 use PDO;
 
+use Throwable;
+
+use function call_user_func;
 use function sprintf;
 
 final class Connection extends Transaction
 {
     private const DSN = 'mysql:dbname=%s;host=%s;port=%d';
+
+    private ?Transaction $transaction = null;
 
     /**
      * @throws Exception
@@ -35,5 +40,37 @@ final class Connection extends Transaction
         }
 
         parent::__construct($pdo);
+    }
+
+    /**
+     * @throws Throwable
+     * @return mixed
+     */
+    public function transaction(callable $func)
+    {
+        $pdo = $this->fetchPDO();
+
+        $pdo->beginTransaction();
+
+        try {
+            $result = call_user_func($func, $this->fetchTransaction());
+        } catch (Throwable $e) {
+            $pdo->rollBack();
+
+            throw $e;
+        }
+
+        $pdo->commit();
+
+        return $result;
+    }
+
+    private function fetchTransaction(): Transaction
+    {
+        if (null === $this->transaction) {
+            $this->transaction = new Transaction($this->fetchPDO());
+        }
+
+        return $this->transaction;
     }
 }
